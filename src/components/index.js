abdbbaeba6ffce5322a1ac36be1a22915a07ed7e
Morgan Balde <morgan.balde@gmail.com>
2023-02-10 11:28:51 +0100

    refactor: :fire: remove buttons examples

--- src/components/index.js
+++
@@ -1,2 +0,0 @@
-export { default as MyButton2 } from './MyButton2.vue'
-export { default as MyButton } from './MyButton.vue'
--- src/components/main.ts
+++ src/components/main.ts
@@ -1,11 +1,8 @@
 import type { App } from 'vue'
-import { MyButton, MyButton2 } from '@/components'
 
 export default {
   install: (app: App) => {
-    app.component('MyButton', MyButton)
-    app.component('MyButton2', MyButton2)
   }
 }
 
-export { MyButton2, MyButton }
+export { }

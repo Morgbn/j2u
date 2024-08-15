ad1f738eb6b1d4c2ffe47a5b20c5b27bc239e1a4
Morgbn <morgan.balde@gmail.com>
2024-08-15 15:04:20 +0200

    chore: :arrow_up: upgrade dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import { createConfigForNuxt } from '@nuxt/eslint-config/flat'
+
+export default createConfigForNuxt({
+  features: {
+    tooling: true,
+    stylistic: {
+      commaDangle: 'never',
+      braceStyle: '1tbs',
+      quoteProps: 'as-needed'
+    }
+  }
+})
+  .append({
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off'
+    }
+  })
